fe30cf76c3bc74bb13cecfce68b5e8db3fd5d514
OccRx <96938516+OccRx@users.noreply.github.com>
2022-04-16 22:15:28 +0300

    Update UserRepository.java

--- src/main/java/my/springboot_mvcjpa_231/Repositories/UserRepository.java
+++ src/main/java/my/springboot_mvcjpa_231/Repositories/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Long>{
 
     User findUserById(Long id);
